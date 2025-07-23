6de762c40a264d9641559f48380a7a9e8a37b775
fernandogbz <fgjv06@gmail.com>
2025-07-23 12:46:05 -0400

    Update UserList component to change header and table column labels

--- frontend/src/pages/Admin/UserList.jsx
+++ frontend/src/pages/Admin/UserList.jsx
@@ -59,7 +59,7 @@ const UserList = () => {
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-semibold mb-4">Users</h1>
+      <h1 className="text-2xl font-semibold ml-28 mb-4">Lista de Usuarios</h1>
       {isLoading ? (
         <Loader />
       ) : error ? (
@@ -73,9 +73,9 @@ const UserList = () => {
             <thead>
               <tr>
                 <th className="px-4 py-2 text-left">ID</th>
-                <th className="px-4 py-2 text-left">NAME</th>
-                <th className="px-4 py-2 text-left">EMAIL</th>
-                <th className="px-4 py-2 text-left">ADMIN</th>
+                <th className="px-4 py-2 text-left">Nombre</th>
+                <th className="px-4 py-2 text-left">Email</th>
+                <th className="px-4 py-2 text-left">Admin</th>
                 <th className="px-4 py-2"></th>
               </tr>
             </thead>
