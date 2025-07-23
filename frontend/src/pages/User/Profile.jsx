80ee1dc61f6e220a732e9b88cbd9b64c217ddb17
fernandogbz <fgjv06@gmail.com>
2025-07-23 11:38:46 -0400

    Refactor user profile form to include additional fields and update labels for improved user experience

--- frontend/src/pages/User/Profile.jsx
+++ frontend/src/pages/User/Profile.jsx
@@ -8,8 +8,12 @@ import { setCredentials } from "../../redux/features/auth/authSlice";
 import { Link } from "react-router-dom";
 
 const Profile = () => {
-  const [username, setUserName] = useState("");
+  const [run, setRun] = useState("");
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
@@ -19,26 +23,42 @@ const Profile = () => {
     useProfileMutation();
 
   useEffect(() => {
-    setUserName(userInfo.username);
+    setRun(userInfo.run);
+    setName(userInfo.name);
+    setLastName(userInfo.lastName);
     setEmail(userInfo.email);
-  }, [userInfo.email, userInfo.username]);
+    setPhone(userInfo.phone);
+    setAddress(userInfo.address);
+  }, [
+    userInfo.run,
+    userInfo.name,
+    userInfo.lastName,
+    userInfo.email,
+    userInfo.phone,
+    userInfo.address,
+  ]);
 
   const dispatch = useDispatch();
 
   const submitHandler = async (e) => {
     e.preventDefault();
+
     if (password !== confirmPassword) {
-      toast.error("Passwords do not match");
+      toast.error("Las contraseñas no coinciden");
     } else {
       try {
         const res = await updateProfile({
           _id: userInfo._id,
-          username,
+          run,
+          name,
+          lastName,
           email,
+          phone,
+          address,
           password,
         }).unwrap();
         dispatch(setCredentials({ ...res }));
-        toast.success("Profile updated successfully");
+        toast.success("Perfil actualizado exitosamente");
       } catch (err) {
         toast.error(err?.data?.message || err.error);
       }
@@ -46,27 +66,48 @@ const Profile = () => {
   };
 
   return (
-    <div className="container mx-auto p-4 mt-[10rem]">
+    <div className="container mx-auto p-4 mt-[5rem]">
       <div className="flex justify-center align-center md:flex md:space-x-4">
         <div className="md:w-1/3">
-          <h2 className="text-2xl font-semibold mb-4">Update Profile</h2>
+          <h2 className="text-2xl font-semibold mb-4">Actualizar Perfil</h2>
           <form onSubmit={submitHandler}>
             <div className="mb-4">
-              <label className="block text-white mb-2">Name</label>
+              <label className="block text-white mb-2">Run</label>
               <input
                 type="text"
-                placeholder="Enter name"
                 className="form-input p-4 rounded-sm w-full"
-                value={username}
-                onChange={(e) => setUserName(e.target.value)}
+                value={run}
+                disabled
               />
             </div>
 
             <div className="mb-4">
-              <label className="block text-white mb-2">Email Address</label>
+              <label className="block text-white mb-2">Nombre</label>
+              <input
+                type="text"
+                placeholder="Ingresa nombre"
+                className="form-input p-4 rounded-sm w-full"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-white mb-2">Apellido</label>
+              <input
+                type="text"
+                placeholder="Ingresa apellido"
+                className="form-input p-4 rounded-sm w-full"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-white mb-2">Email</label>
               <input
                 type="email"
-                placeholder="Enter email"
+                placeholder="Ingresa email"
                 className="form-input p-4 rounded-sm w-full"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -74,10 +115,32 @@ const Profile = () => {
             </div>
 
             <div className="mb-4">
-              <label className="block text-white mb-2">Password</label>
+              <label className="block text-white mb-2">Teléfono</label>
+              <input
+                type="text"
+                placeholder="Ingresa teléfono"
+                className="form-input p-4 rounded-sm w-full"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-white mb-2">Dirección</label>
+              <input
+                type="text"
+                placeholder="Ingresa dirección de envios"
+                className="form-input p-4 rounded-sm w-full"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-white mb-2">Contraseña</label>
               <input
                 type="password"
-                placeholder="Enter password"
+                placeholder="Ingresar contraseña"
                 className="form-input p-4 rounded-sm w-full"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -85,10 +148,12 @@ const Profile = () => {
             </div>
 
             <div className="mb-4">
-              <label className="block text-white mb-2">Confirm Password</label>
+              <label className="block text-white mb-2">
+                Confirmar Contraseña
+              </label>
               <input
                 type="password"
-                placeholder="Confirm password"
+                placeholder="Repite la contraseña ingresada"
                 className="form-input p-4 rounded-sm w-full"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
@@ -100,14 +165,14 @@ const Profile = () => {
                 type="submit"
                 className="bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-600"
               >
-                Update
+                Actualizar
               </button>
 
               <Link
                 to="/user-orders"
                 className="bg-pink-600 text-white py-2 px-4 rounded hover:bg-pink-700"
               >
-                My Orders
+                Mis Pedidos
               </Link>
             </div>
             {loadingUpdateProfile && <Loader />}
